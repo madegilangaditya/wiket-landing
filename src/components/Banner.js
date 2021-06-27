5f39c964b663dad1cc6b79dd7019109e0a94928c
gilang aditya <madegilangaditya32@gmail.com>
2021-06-27 11:08:48 +0800

    add mobile menu popup and menu fixed when scrolled

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,16 @@
+import florist from '../jpg/header-img-florist.jpg'
+import gardener from '../jpg/header-img-gardener.jpg'
+import pottery from '../jpg/header-img-pottery.jpg'
+import barista from '../jpg/header-img-barista.jpg'
+import line from '..//svg/header-line-phone-portrait.svg'
+
 const Banner = () => {
     return (
         <div className="banner-section">
             <div className="container">
                 <div className="col">
                     <div className="text-content">
-                        <p className="subtitle">Explore new opportunities. </p>
-                        <h1><span>Grow</span> your business.</h1>
+                        <h1><span className="subtitle">Explore new opportunities. </span><strong>Grow</strong> your business.</h1>
                         <p><strong>Wiket is the first business to business network</strong> which lets you connect to mind like people.</p>
                     </div>
                 </div>
@@ -15,30 +20,33 @@ const Banner = () => {
                             <p className="name">Hanging Garden</p>
                             <p className="job-title">Florist in Bangkok</p>
                         </div>
-                        <img src="../jpg/header-img-florist.jpg" alt="Florist" />
+                        <img src={florist} alt="Florist" />
                     </div>
                     <div className="profile-wrap">
                         <div className="profile-card card-1">
-                            <img src="../jpg/header-img-pottery.jpg" alt="Pottery" />
+                            <img src={pottery} alt="Pottery" />
                             <div className="profile-text">
                                 <p className="name">Potject</p>
                                 <p className="job-title">Pottery in Bangkok</p>
                             </div>
                         </div>
                         <div className="profile-card card-2">
-                            <img src="../jpg/header-img-pottery.jpg" alt="Pottery" />
+                            <img src={gardener} alt="Gardener" />
                             <div className="profile-text">
                                 <p className="name">GreenThumb</p>
                                 <p className="job-title">Gardener in CheChe</p>
                             </div>
                         </div>
                         <div className="profile-card card-3">
-                            <img src="../jpg/header-img-pottery.jpg" alt="Pottery" />
+                            <img src={barista} alt="Barista" />
                             <div className="profile-text">
                                 <p className="name">Green Cafe</p>
                                 <p className="job-title">Cafe in Bangkok</p>
                             </div>
                         </div>
+                    </div>
+                    <div className="bg-image">
+                        <img src={line} alt="Line" />
                     </div>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
 import logo from '../svg/wiket-logo.svg'
+import React, { useEffect } from 'react'
 
 const Header = () => {
+    const [scrolled, setScrolled] = React.useState(false)
+
+    const menuClick= () => {
+        document.querySelector('.menu').classList.toggle('active')
+    }
+
+    // Menu change background when scrolled
+    const handleScroll=() => {
+        const offset=window.scrollY;
+        if(offset > 100 ){
+            setScrolled(true);
+        }
+        else{
+            setScrolled(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        document.querySelector('.mobile-menu-icon').addEventListener('click', menuClick);
+    },[]);
+
     return (
-        <header className="App-header">
+        <header className={`App-header ${scrolled ? 'scrolled' : ''}`}>
             <div className="container">
                 <div className="col col-left">
-                    <img src={logo} alt="Logo" />
+                    <img className="logo" src={logo} alt="Logo" />
                 </div>
                 <div className="col col-right">
-                    <nav>
-                    <ul>
-                        <li><a href="#root">Benefits</a></li>
-                        <li><a href="#root">Your Profile</a></li>
-                        <li><a href="#root">Connections</a></li>
-                        <li><a href="#root">Plans & Pricing</a></li>
-                    </ul>
+                    <nav className="menu">
+                        <ul>
+                            <li><a href="#root">Benefits</a></li>
+                            <li><a href="#root">Your Profile</a></li>
+                            <li><a href="#root">Connections</a></li>
+                            <li><a href="#root">Plans & Pricing</a></li>
+                        </ul>
                     </nav>
                     <a href="#root" className="btn">Get Started</a>
                     <div className="mobile-menu-icon">
